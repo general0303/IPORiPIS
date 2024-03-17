59cad46a99ab438693d0381d3e460b3db52ccb0f
general0303 <psh030302@gmail.com>
2024-03-17 15:02:01 +0300

    calculator

--- pr5/main.py
+++ pr5/main.py
@@ -2,14 +2,62 @@ import sys
 from PyQt5 import QtWidgets
 import design
 
-import numexpr as ne
+OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y),
+             '^': (3, lambda x, y: x ** y)}
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.btn.clicked.connect(lambda: self.resultLabel.setText(f"Результат: {ne.evaluate(self.plane.toPlainText())}"))
+        self.btn.clicked.connect(lambda: self.resultLabel.setText(f"Результат: {self.__calc(self.__shunting_yard(self.__parse(self.plane.toPlainText())))}"))
+
+    @staticmethod
+    def __parse(formula_string):
+        number = ''
+        for s in formula_string:
+            if s in '1234567890.':
+                number += s
+            elif number:
+                yield float(number)
+                number = ''
+            if s in OPERATORS or s in "()":
+                yield s
+        if number:
+            yield float(number)
+
+    @staticmethod
+    def __shunting_yard(parsed_formula):
+        stack = []
+        for token in parsed_formula:
+            if token in OPERATORS:
+                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+    @staticmethod
+    def __calc(polish):
+        stack = []
+        for token in polish:
+            if token in OPERATORS:
+                y, x = stack.pop(), stack.pop()
+                stack.append(OPERATORS[token][1](x, y))
+            else:
+                stack.append(token)
+        return stack[0]
 
 
 def main():
